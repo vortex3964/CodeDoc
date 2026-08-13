498a9daffb6c738b229fce46ad273ea03e967cc0
vortex3964 <theo2005.adams@gmail.com>
2026-08-14 01:45:41 +0300

    read path recursively and new exclude list
    we added lists of common extensions files and dirs we
    should avoid reading and then we created a function
    that walks the filepaths with the os import and catalogs
    the files that are ok to read and adds them in a list we then sort
    and return to main this list is the list of the files were allowed
    to parse in our parser

--- main.py
+++ main.py
@@ -1,9 +1,129 @@
 # DESC: main.py is purely here to process the flags and start the procedure
 # for the files in the right mode
 
-def main():
-    print("hello world")
+import os
+from pathlib import Path
 
+#  list of ignored files extensions etch
+IGNORE_DIRS = {
+    ".git",
+    ".svn",
+    ".hg",
+    "node_modules",
+    "vendor",
+    "bower_components",
+    ".venv",
+    "venv",
+    "env",
+    "__pycache__",
+    "dist",
+    "build",
+    "out",
+    "target",
+    ".next",
+    ".nuxt",
+    "bin",
+    "obj",
+    ".cache",
+    ".pytest_cache",
+    ".mypy_cache",
+    ".ruff_cache",
+    ".tox",
+    ".idea",
+    ".vscode",
+    ".vs",
+    ".gradle",
+    ".terraform",
+    "coverage",
+    "site-packages",
+}
+
+IGNORE_EXTENSIONS = {
+    ".pyc",
+    ".pyo",
+    ".class",
+    ".o",
+    ".so",
+    ".dll",
+    ".dylib",
+    ".exe",
+    ".zip",
+    ".tar",
+    ".gz",
+    ".tgz",
+    ".rar",
+    ".7z",
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".gif",
+    ".bmp",
+    ".ico",
+    ".webp",
+    ".mp3",
+    ".mp4",
+    ".wav",
+    ".avi",
+    ".mov",
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+    ".ttf",
+    ".otf",
+    ".woff",
+    ".woff2",
+    ".db",
+    ".sqlite",
+    ".sqlite3",
+    ".bin",
+    ".lock",
+    ".mod",
+}
+
+IGNORE_FILES = {
+    ".DS_Store",
+    "Thumbs.db",
+    "package-lock.json",
+    "yarn.lock",
+    "poetry.lock",
+    "Pipfile.lock",
+    "Cargo.lock",
+}
+
+
+# recursively list all the files under root dir
+def list_files_req(root_dir , exc):
+    files = []
+
+    for dirpath, dirnames , filenames in os.walk(root_dir):
+        
+        dirnames[:] = [d for d in dirnames if (d not in exc and d not in IGNORE_DIRS)]
+
+        for fname in filenames:
+            if fname in IGNORE_FILES:
+                continue
+
+            ext = Path(fname).suffix.lower()
+
+            if ext in IGNORE_EXTENSIONS:
+                continue
+
+            files.append(os.path.join(dirpath, fname))
+
+    return files.sort()
+
+
+def main():
+    #exclude list leave till we add flags
+    exc = []
+    #current working dir needs to change
+    path = Path().parent.resolve()
+    print(path)
+    list_files_req(path,exc)
 
 if __name__ == "__main__":
     main()
---
+++ parser.py
@@ -0,0 +1,1 @@
+# DESC: this file has the thread pool and logic to parse files looking for comments with the right mode deppending on the flags
